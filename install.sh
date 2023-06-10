6c4c83bcc634418eb164541908408127c5b117a9
beskay <beskay0x@gmail.com>
2023-06-10 15:29:47 +0200

    Update version to v0.2.3

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ APP_NAME="evm-runners"
 APP_NAME_ALT="evmr"
 GITHUB_USER="ethernautdao"
 GITHUB_REPO="evm-runners-cli"
-VERSION="v0.2.2"
+VERSION="v0.2.3"
 
 PLATFORM="$(uname -s)"
 case $PLATFORM in
