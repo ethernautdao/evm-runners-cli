3027dbd0552e079ebc52f874c882567bcd70c711
beskay <beskay0x@gmail.com>
2023-08-25 18:27:56 +0200

    :sparkles: Add update cmd to update levels

---
+++ cmd/update.go
@@ -0,0 +1,43 @@
+package cmd
+
+import "github.com/ethernautdao/evm-runners-cli/internal/utils"
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+)
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update evm-runners levels",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// load config
+		config, err := utils.LoadConfig()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Updating evm-runners levels...")
+		if _, err := os.Stat(config.EVMR_LEVELS_DIR); os.IsNotExist(err) {
+			return fmt.Errorf("evm-runners directory not found, run 'evmr init' first!\n")
+		} else {
+			execCmd := exec.Command("git", "pull")
+			execCmd.Stdout = os.Stdout
+			execCmd.Stderr = os.Stderr
+			execCmd.Dir = config.EVMR_LEVELS_DIR
+			if err := execCmd.Run(); err != nil {
+				return fmt.Errorf("error updating the level directory: %v", err)
+			}
+		}
+		//fmt.Println("evm-runners updated successfully")
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
