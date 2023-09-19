bebde3fb8edb0eaf7a86fc95024f41656dceca17
beskay <beskay0x@gmail.com>
2023-09-20 00:04:20 +0200

    :books: Update evmr description

--- cmd/about.go
+++ cmd/about.go
@@ -19,21 +19,22 @@ var aboutCmd = &cobra.Command{
  \___| \_/ |_| |_| |_|     |_|   \__,_|_| |_|_| |_|\___|_|  |___/
                                                                  `)
 
-		fmt.Println("A terminal-based game for developers with EVM-based challenges")
+		fmt.Println("A terminal-based game for developers with EVM-based levels")
 		fmt.Println("\nSponsored by \x1b[94m@EthernautDAO\x1b[0m and \x1b[94m@Optimism\x1b[0m")
 		fmt.Println("Authors: \x1b[94m@0xkarmacoma\x1b[0m, \x1b[94m@beskay0x\x1b[0m, \x1b[94m@kyre_rs\x1b[0m")
 		fmt.Println("")
 		fmt.Println("Website: \x1b[94mhttps://evmr.sh\x1b[0m")
-		fmt.Println("Discord: \x1b[94mhttps://discord.gg/RQ5WYDxUF3\x1b[0m")
+		fmt.Println("Discord: \x1b[94mhttps://discord.gg/2TwURWvnVT\x1b[0m")
 
-		fmt.Println("\nevm-runners is not just a regular CTF:")
-		fmt.Println(" - Gradual introduction to the EVM")
-		fmt.Println(" - Score based, not hack based")
-		fmt.Println(" - Dual scores (gas and codesize), not a compound score")
-		fmt.Println(" - Linear progression: Challenges get more complex as you progress")
-		fmt.Println(" - Multiplayer: Compare your scores with other players")
-		fmt.Println(" - Singleplayer: Play at your own pace, learn usefull skills")
-		fmt.Println(" - Work on solutions in a language of your choice (Solidity, Vyper, Yul, Huff)")
+		fmt.Println("\nevm-runners is more than your typical CTF game:")
+		fmt.Println("")
+		fmt.Println("  - No time limit")
+		fmt.Println("  - Score based, not hack based")
+		fmt.Println("  - Dual scores (gas and codesize), not a compound score")
+		fmt.Println("  - Linear progression: Levels get more complex as you progress")
+		fmt.Println("  - Work in a language of your choice (Solidity, Vyper, Yul, Huff)")
+		fmt.Println("  - Singleplayer: Play at your own pace and acquire usefull skills")
+		fmt.Println("  - Multiplayer: Benchmark your scores against other players")
 		fmt.Println("")
 	},
 }
